f8f32003ab50c18ab0fda2335522524140390436
rufus1530 <rafal.dardzinski@gmail.com>
2018-05-18 21:44:16 +0200

    Shell scripts basics

---
+++ shell-scripting/myscript.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/bash
+
+# ECHO Command
+# echo Hello world!
+
+# #variables
+# # Uppercase convention
+# NAME="Andy"
+
+# echo "My name is $NAME"
+
+# #USER INPUT
+# read -p "Enter your name: " U_NAME
+# echo "Hello $U_NAME, nice to meet you"
+
+#conditionals
+# NAME="Brad"
+# if [ "$NAME" == "Brad" ]
+# then 
+#     echo "your name is Brad"
+# fi
+
+# read -p "enter your name: " NAME
+# if [ "$NAME" == "Brad" ]
+# then
+#     echo "Your name is Brad"
+# else
+#     echo "Your name is not Brad"
+# fi
+
+# else-if (elif)
+# read -p "enter your name: " NAME
+# if [ "$NAME" == "Brad" ]
+# then
+#     echo "Your name is Brad"
+# elif [ "$NAME" == "jack" ]
+# then
+#     echo "Your name is jack"
+# else
+#     echo "Your name is not Brad nor jack"
+# fi
+
+# COMPARISON
+# NUM1=31
+# NUM2=5
+
+# if [ "$NUM1" -gt "$NUM2" ]
+# then 
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
