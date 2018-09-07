5a1a68a1d43e4907e9a6ac1bf497daa10768eae7
rufus1530 <rafal.dardzinski@gmail.com>
2018-09-07 14:37:51 +0200

    removed console.logs and typos.

--- js/distributeElementsInsideContainer.js
+++ js/distributeElementsInsideContainer.js
@@ -5,21 +5,18 @@
 */
 
 function distributeElements(container, elements) {
-  var radius = getElementRadius(container);
-  var width = container.offsetWidth,
-    height = container.offsetHeight,
-    angle = 180, step = (2*Math.PI) / elements.length;
+    var radius = getElementRadius(container);
+    var width = container.offsetWidth,
+        height = container.offsetHeight,
+        angle = 180, step = (2*Math.PI) / elements.length;
     
-    console.log('width: ' + width + '\nheight: ' + height);
-    
-    for (var i = 0; i < fields.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
         var x = Math.round(width / 2 + radius * Math.cos(angle) - elements[i].offsetWidth / 2);
         var y = Math.round(height / 2 + radius * Math.sin(angle) - elements[i].offsetHeight / 2);
         
         elements[i].style.left = x + 'px';
         elements[i].style.top = y + 'px';
         
-        console.log('x: ' + x + '\ny: ' + y);
         angle += step;
     }
     
